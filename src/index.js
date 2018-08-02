a60ab81c4e719f2cf5d685c66a1b7cd4648f4cc3
Rob Gaston <rgaston@fargeo.com>
2018-08-02 14:41:56 -0700

    deploying latest...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './styles.css';
-import * as token from './token';
+import './token';
 import * as mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken = token;
+import style from './styles/mapbox_streets_v8.json';
+import places from './data/la_historic_places.json';
+import neighborhoods from './data/la-county-neighborhoods-v6.json';
 
-let map = new mapboxgl.Map({
+style.sources = Object.assign(style.sources, {
+    'places': {
+        'type': 'geojson',
+        'data': places
+    },
+    'neighborhoods': {
+        'type': 'geojson',
+        'data': neighborhoods
+    }
+});
+
+style.layers.push({
+    'id': 'places',
+    'type': 'heatmap',
+    'source': 'places'
+}, {
+    'id': 'neighborhoods',
+    'type': 'fill',
+    'source': 'neighborhoods',
+    'layout': {},
+    'paint': {
+        'fill-color': '#088',
+        'fill-opacity': 0.1,
+        'fill-outline-color': 'black'
+    }
+});
+
+new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: style,
+    center: [-118.5, 34],
+    zoom: 9
 });
--- src/token.js
+++ src/token.js
@@ -1,1 +1,3 @@
-module.exports = 'pk.eyJ1Ijoicmdhc3RvbiIsImEiOiJJYTdoRWNJIn0.MN6DrT07IEKXadCU8xpUMg';
+import * as mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicmdhc3RvbiIsImEiOiJJYTdoRWNJIn0.MN6DrT07IEKXadCU8xpUMg';
